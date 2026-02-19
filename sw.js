82b3451187a78166d28c0bc681e8646faa004f2b
Angelos2024 <skyblackroan@gmail.com>
2026-02-19 10:46:25 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,60 @@
+const CACHE_NAME = 'cuaderno-static-v1';
+const OFFLINE_ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './offline/vosk/vosk.js',
+  './offline/vosk/vosk.wasm',
+  './offline/model.tar.gz',
+  './offline/vosk-model-small-es-0.42/README',
+  './offline/vosk-model-small-es-0.42/am/final.mdl',
+  './offline/vosk-model-small-es-0.42/conf/mfcc.conf',
+  './offline/vosk-model-small-es-0.42/conf/model.conf',
+  './offline/vosk-model-small-es-0.42/graph/Gr.fst',
+  './offline/vosk-model-small-es-0.42/graph/HCLr.fst',
+  './offline/vosk-model-small-es-0.42/graph/disambig_tid.int',
+  './offline/vosk-model-small-es-0.42/graph/phones/word_boundary.int',
+  './offline/vosk-model-small-es-0.42/ivector/final.dubm',
+  './offline/vosk-model-small-es-0.42/ivector/final.ie',
+  './offline/vosk-model-small-es-0.42/ivector/final.mat',
+  './offline/vosk-model-small-es-0.42/ivector/global_cmvn.stats',
+  './offline/vosk-model-small-es-0.42/ivector/online_cmvn.conf',
+  './offline/vosk-model-small-es-0.42/ivector/splice.conf',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => Promise.allSettled(OFFLINE_ASSETS.map((asset) => cache.add(asset))))
+      .then(() => self.skipWaiting()),
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys
+        .filter((key) => key !== CACHE_NAME)
+        .map((key) => caches.delete(key))))
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+
+      return fetch(event.request)
+        .then((response) => {
+          const cloned = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, cloned));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'));
+    }),
+  );
+});
